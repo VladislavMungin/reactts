b6a28d6de474b8934be8d3f53829ddf8bf139ee6
Vladislav Mungin <93925061+VladislavMungin@users.noreply.github.com>
2023-11-04 14:55:47 +0300

    Add files via upload

---
+++ precache-manifest.4e7ae0a1360963c795bc7c8322a02309.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d319b080ed64a676557717a40095414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dc740efb7da49284dc7",
+    "url": "/static/js/2.6e8c7cc7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6e8c7cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4bd64b050300a4a093",
+    "url": "/static/js/main.dc561053.chunk.js"
+  },
+  {
+    "revision": "7b88e63c87bb1dd1f126",
+    "url": "/static/js/runtime-main.47d1f893.js"
+  },
+  {
+    "revision": "98878a7fb6c4d11f006f5f0a48816bae",
+    "url": "/static/media/Kontur-Iconic-eco-20px.98878a7f.woff2"
+  },
+  {
+    "revision": "b811ce6cc66ff93ba6ba6dd225b475df",
+    "url": "/static/media/Kontur-Iconic-eco-20px.b811ce6c.eot"
+  },
+  {
+    "revision": "cc8e65cb2d3a893285466730d7b4f022",
+    "url": "/static/media/Kontur-Iconic-eco-20px.cc8e65cb.woff"
+  },
+  {
+    "revision": "7d850235c4b0a12aed1c0d83e489acd5",
+    "url": "/static/media/fallback_circle.7d850235.png"
+  },
+  {
+    "revision": "7890347bd63a549c5164f53487b8031f",
+    "url": "/static/media/fallback_circle_big.7890347b.png"
+  },
+  {
+    "revision": "dae3133efdcd89ac9e4d3335d2675b1d",
+    "url": "/static/media/fallback_circle_normal.dae3133e.png"
+  },
+  {
+    "revision": "7b359e7d9365a987d17524f81fc66fb5",
+    "url": "/static/media/font.7b359e7d.woff2"
+  },
+  {
+    "revision": "dc62e0fc93a9b5469860b6a22338562d",
+    "url": "/static/media/font.dc62e0fc.eot"
+  },
+  {
+    "revision": "f01e8d1506e8cb4cbb1f1c39dab9dab9",
+    "url": "/static/media/font.f01e8d15.woff"
+  }
+]);
